0eab53a0b02c3193772482931b88197870b933de
genafalconi <genaro.f@hotmail.com>
2022-06-21 13:31:59 -0300

    'Fc'

---
+++ src/components/mainContent/ProductsMap.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductCard from './ProductCard'
+
+export default function ProductsMap({ products }) {
+    return (
+        <>
+            {
+                products?.map((prod) => prod.productIsActive && prod.productStock > 0 && <ProductCard key={prod._id} {...prod} product={prod} />)
+            }
+        </>
+    )
+}
